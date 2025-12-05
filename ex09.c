d20cc110bf5d3539fa96b4de1bfd0056546b8a99
Ali Şer <aliseroncel@outlook.com>
2025-12-05 18:41:20 +0300

    .

--- ex09.c
+++ ex09.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
 
-int main()
-{
-    for (int i = 1; i <= 3; i++)
-    {
-        for (int j = 1; j <= 10; j++)
-        {
-            printf("%d", i * j);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
