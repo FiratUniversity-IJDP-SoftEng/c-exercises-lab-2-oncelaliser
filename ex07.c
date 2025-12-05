26b582716c0def4bb4eb7a303a781d956891b70f
Ali Şer <aliseroncel@outlook.com>
2025-12-05 19:57:20 +0300

    Lab2 ex7
    Add a loop to print formatted columns of values.

--- ex07.c
+++ ex07.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(){
+    for(int i = 1; i < 10; i++){
+        int column1 = i*5;
+        int column2 = 100 - (i*10 - 9);
+        int column3 = i;
+        printf("%d \n", column1);
+        printf("\t %d \n", column2);
+        printf("\t \t %d \n", column3);
+    }
+    
+    
+    return 0;
+}
