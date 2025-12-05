dcd33020a3c034fd3b9718b06419f066b71af1cc
Ali Şer <aliseroncel@outlook.com>
2025-12-05 19:26:21 +0300

    Lab2 ex12
    Implement factorial function with input handling

--- ex12.c
+++ ex12.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int fact(int a){
+    if(a == 1){
+        return 1;
+    }
+    else if(a > 0 && a < 50){
+        return(a * fact(a - 1));
+    }
+    else{
+        return -1;
+    }
+}
+
+int main(){
+    int a;
+    printf("Enter a number for factorial:");
+    scanf("%d", &a);
+    printf("%d", fact(a));
+    
+}
