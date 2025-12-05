8b02e3da597914625b93b0f06b7e0d221a8a24ad
Ali Şer <aliseroncel@outlook.com>
2025-12-05 19:08:42 +0300

    Lab2 ex11
    Add a program to print a diamond shape pattern.

--- ex11.c
+++ ex11.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    int i;
+    int j;
+    int c;
+
+    
+    int a = 5;  
+    c = a * 2;  
+
+   
+    for (i = 0; i < 2 * a - 1; i++) {
+        
+        
+        for (j = 0; j < 2 * a - 1; j++) {
+            
+
+            if (j == i || j == 2 * a - 2 - i)
+                printf("*");  
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
