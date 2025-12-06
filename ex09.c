93be61becd184c7f6d2e8e155e0be95cfb162cf9
Ali Şer <aliseroncel@outlook.com>
2025-12-06 15:03:38 +0300

    Lab2 ex9
    This program calculates the sum, count, and average of user-entered integers until -1 is entered.

--- ex09.c
+++ ex09.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    int a;
+    int sum = 0;
+    int count = 0;
+    float average;
+
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &a);
+
+        if (a != -1) {
+            count++;
+            sum += a;
+        }
+
+    } while (a != -1);
+
+    if (count > 0) {
+        average = (float)sum / count;
+        printf("Count = %d\n", count);
+        printf("Sum = %d\n", sum);
+        printf("Average = %.2f\n", average);
+    }
+    else {
+        printf("You haven't entered any numbers.\n");
+    }
+
+    return 0;
+}
