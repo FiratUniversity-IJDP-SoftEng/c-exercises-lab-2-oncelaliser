ae1b55740d021c217dbb804be4373627f12659e1
Ali Şer <aliseroncel@outlook.com>
2025-12-06 14:08:02 +0300

    Update ex13.c
    Implement factorial function with input validation

--- ex13.c
+++ ex13.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+//It is same with ex12 both of them are recursive functions
+int fact(int a){
+    if(a == 1){
+        return 1;
+    }
+    else if(a > 0 && a < 50){
+        return(a * fact(a - 1));
+    }
+    else{
+        return -1;
+    }
+}
 
+int main(){
+    int a;
+    printf("Enter a number for factorial:");
+    scanf("%d", &a);
+    printf("%d", fact(a));
+    
+}
